b78a6a075945ebf84a7ce5e281dd5fd6e84cecb2
Riaz <riazaham@gmail.com>
2021-08-11 14:26:16 +0800

    Create API calls with superagent

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { getDate } from "./date.js";
-import https from "https";
-import { getRecords } from "./records.js";
+import { getRecord } from "./records.js";
 
 const app = express();
 app.set("view-engine", "ejs");
@@ -15,11 +14,10 @@ app.get("/", (req, res) => {
 	res.render("home.ejs", { date: date });
 });
 
-app.get("/search", (req, res) => {
-	const records = [];
-	getRecords(records, searchWord, () =>
-		res.render("search.ejs", { records: records })
-	);
+app.get("/search", async (req, res) => {
+	const record = await getRecord(searchWord);
+	console.log(record);
+	res.render("search.ejs", { record: record });
 });
 
 app.post("/search", (req, res) => {
@@ -28,3 +26,8 @@ app.post("/search", (req, res) => {
 });
 
 app.listen(3000, () => console.log("Server started on port 3000."));
+
+//OneMAP api code
+//JDJ5JDEwJG5WRDg0VzRaYlZZaEZkUjFXdmNaeWV2UjNTYzVQMW5salIzM1pvNXY2aGpRQjJOTXNxaklD
+//OneMAP token
+//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjc4NjAsInVzZXJfaWQiOjc4NjAsImVtYWlsIjoicmlhemFoYW1AZ21haWwuY29tIiwiZm9yZXZlciI6ZmFsc2UsImlzcyI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0IjoxNjI4NjUzNjg5LCJleHAiOjE2MjkwODU2ODksIm5iZiI6MTYyODY1MzY4OSwianRpIjoiYzMwMjFmNzQ5MTI5ZWI5MzBiZjU1Y2YzOGY2ZGY3NmYifQ.-5XuVukSctHRhoWAYz6YVeHK3DkNOWGRH65l-VCKC2o
--- records.js
+++ records.js
@@ -1,29 +1,40 @@
-import https from "https";
+import superagent from "superagent";
 
 const getCarparkRateURL =
 	"https://data.gov.sg/api/action/datastore_search?resource_id=85207289-6ae7-4a56-9066-e6090a3684a5&limit=1";
 
-export function getRecords(records, searchWord, callback) {
-	const query = "&q=" + searchWord;
-	https.get(getCarparkRateURL + query, (response) => {
-		const chunks = [];
+const getLatLongURL =
+	"https://developers.onemap.sg/commonapi/search?returnGeom=Y&getAddrDetails=N&pageNum=1";
 
-		response.on("data", (chunk) => {
-			chunks.push(chunk);
-		});
+const makeRequest = async (url, queries) => {
+	const res = await superagent.get(url).query(queries);
+	return JSON.parse(res.res.text);
+};
 
-		response.on("end", () => {
-			const data = JSON.parse(Buffer.concat(chunks));
-			for (let record of data.result.records) {
-				records.push({
-					carpark: record.carpark,
-					weekdays_rate_1: record.weekdays_rate_1,
-					weekdays_rate_2: record.weekdays_rate_2,
-					saturday_rate: record.saturday_rate,
-					sunday_publicholiday_rate: record.sunday_publicholiday_rate,
-				});
-			}
-			callback();
-		});
-	});
+export async function getRecord(searchWord) {
+	const carparkRatesQuery = "&q=" + searchWord;
+	let carparkRatesData = await makeRequest(
+		getCarparkRateURL,
+		carparkRatesQuery
+	);
+	carparkRatesData = carparkRatesData.result.records[0];
+
+	const carparkLatLongQuery = "&searchVal=" + carparkRatesData.carpark;
+	let carparkLatLongData = await makeRequest(
+		getLatLongURL,
+		carparkLatLongQuery
+	);
+	carparkLatLongData = carparkLatLongData.results[0];
+
+	const record = {
+		carpark: carparkRatesData.carpark,
+		weekdays_rate_1: carparkRatesData.weekdays_rate_1,
+		weekdays_rate_2: carparkRatesData.weekdays_rate_2,
+		saturday_rate: carparkRatesData.saturday_rate,
+		sunday_publicholiday_rate: carparkRatesData.sunday_publicholiday_rate,
+		lat: carparkLatLongData.LATITUDE,
+		long: carparkLatLongData.LONGITUDE,
+	};
+
+	return record;
 }
