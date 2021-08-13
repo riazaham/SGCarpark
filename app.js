1726605d2e8dc42e959a1004b4bb70915f49a304
Riaz <riazaham@gmail.com>
2021-08-14 00:27:47 +0800

    Fix CSS not served issue

--- app.js
+++ app.js
@@ -7,20 +7,19 @@ app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let searchWord = "";
-
 app.get("/", async (req, res) => {
 	const date = getDate();
-	const record = await getRecord(searchWord);
+	const record = await getRecord("");
 	res.render("home.ejs", {
 		date: date,
 		record: record,
-		searchWord: searchWord,
+		searchWord: "",
 	});
 });
 
 app.get("/search/:searchWord", async (req, res) => {
 	const date = getDate();
+	const searchWord = req.params.searchWord;
 	const record = await getRecord(searchWord);
 	res.render("home.ejs", {
 		date: date,
@@ -30,13 +29,7 @@ app.get("/search/:searchWord", async (req, res) => {
 });
 
 app.post("/search", (req, res) => {
-	searchWord = req.body.searchWord;
-	res.redirect("/search/" + searchWord);
+	res.redirect("/search/" + req.body.searchWord);
 });
 
 app.listen(3000, () => console.log("Server started on port 3000."));
-
-//OneMAP api code
-//JDJ5JDEwJG5WRDg0VzRaYlZZaEZkUjFXdmNaeWV2UjNTYzVQMW5salIzM1pvNXY2aGpRQjJOTXNxaklD
-//OneMAP token
-//eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjc4NjAsInVzZXJfaWQiOjc4NjAsImVtYWlsIjoicmlhemFoYW1AZ21haWwuY29tIiwiZm9yZXZlciI6ZmFsc2UsImlzcyI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0IjoxNjI4NjUzNjg5LCJleHAiOjE2MjkwODU2ODksIm5iZiI6MTYyODY1MzY4OSwianRpIjoiYzMwMjFmNzQ5MTI5ZWI5MzBiZjU1Y2YzOGY2ZGY3NmYifQ.-5XuVukSctHRhoWAYz6YVeHK3DkNOWGRH65l-VCKC2o
